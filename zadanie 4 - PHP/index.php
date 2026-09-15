79a6846de751132fc02b3751155122ce407da98e
Arsenii Kozyr <akozyr@technikumlodz.pl>
2026-09-15 13:29:02 +0200

    zadanie 4

---
+++ zadanie 4 - PHP/index.php
@@ -0,0 +1,41 @@
+<?php
+    // $a=5;
+    // if($a%2==0){
+    //     echo"Liczba parzysta";
+    // }else{
+    //     echo"Liczba nie parzysta";
+    // }
+
+    // $a=2;
+    // $b=8;
+    // if($a%$b==0){
+    //     echo"Jest podzielna przez drugą liczbę";
+    // }else{
+    //     echo"Nie jest podzielna przez drugą liczbę";
+    // }
+
+    // $a=17;
+    // if(($a>=1 && $a<=10)||($a>=17&&$a<=21)){
+    //     echo"Należy do przedziału";
+    // }else{
+    //     echo"Nie należy do przedziału";
+    // }
+
+    // $a=5;
+    // if($a>0){
+    //     echo"Większa od zera";
+    // }else if($a==0){
+    //     echo"Równa się zero";
+    // }else{
+    //     echo"Mniejsza od zera";
+    // }
+
+    $wiek=52;
+    if($wiek<11){
+        echo"dzieciak";
+    }else if($wiek>=11&&$wiek<=17){
+        echo"nastolatek";
+    }else{
+        echo"dorosły";
+    }
+?>
