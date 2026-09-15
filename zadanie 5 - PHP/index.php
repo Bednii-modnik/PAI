20b0a17afc43b9a94f5e9c3cfecaec8518fc6d00
Arsenii Kozyr <akozyr@technikumlodz.pl>
2026-09-15 14:17:22 +0200

    zadanie 5

---
+++ zadanie 5 - PHP/index.php
@@ -0,0 +1,65 @@
+<?php
+    // for($i = 0;$i <10;$i++){
+    //     echo $i;
+    // }
+
+    // $i = 0;
+    // while($i < 5){
+    //     echo $i;
+    //     $i++;
+    // }
+
+    // $a=1;
+    // do{
+    //     echo $a;
+    //     $a++;
+    // }while($a <=5);
+
+    // $tab=[1,2,3,4,5];
+    // foreach($tab as $wart){
+    //     echo $wart;
+    // }
+
+    // $owoce=[
+    //     "a"=>"jabłko",
+    //     "b"=>"banan",
+    //     "c"=>"gruszka",
+    //     "d"=>"śliwka",
+
+    // ];
+
+    // foreach($owoce as $klucz => $wart ){
+    //     echo " klucz: ".$klucz." Wartość: ".$wart;    
+    // }
+
+    // $array=[1,2,3];
+    // $assoc_table=['imie'=>"ania","wiek"=>30];
+    // $empty_table=[];
+    // $array2=array(1,2,3);
+    // for($i = 0;$i<count($array);$i++){
+    //     echo $array[$i];
+    // }
+
+    // $number =10;
+    // $insert_array=[1,2,3];
+    // $insert_array[1]=$number;
+
+    // $number =10;
+    // $insert_array=[1,2,3];
+    // for($i = 0;$i<count($insert_array);$i++){
+    //     $insert_array[$i]=$number;
+    // }
+    // echo var_dump($insert_array);
+
+    // $number =10;
+    // $index = 0;
+    // $insert_array=[1,2,3];
+    // $insert_array[$index]=$number;
+
+    for($i=0;$i<=100;$i+=2){
+        echo $i." ";
+    }
+    for($i=100;$i>=1;$i-=4){
+        echo $i." ";
+    }
+?>
